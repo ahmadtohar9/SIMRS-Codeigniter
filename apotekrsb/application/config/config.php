4b77e866c12b1ff389f16208f91520f7611bc849
AhmadTohar <tohargraciaz9@gmail.com>
2023-08-05 16:56:18 +0700

    Update config.php

--- apotekrsb/application/config/config.php
+++ apotekrsb/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.1.2.155/apotekrsb/';
+$config['base_url'] = 'isi url masing-masing';
 
 /*
 |--------------------------------------------------------------------------
